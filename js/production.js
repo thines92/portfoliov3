87e72bdee1551d172cd2a2f0347fc9c6c32871a1
Tyler <tylerhines1992@gmail.com>
2015-12-28 16:55:15 -0500

    Add Owl Slider

--- js/production.js
+++ js/production.js
@@ -15,3 +15,27 @@ $("li").mouseleave(function() {
 // $(".modalImg").hover(function() {
 // 	$(this).css({"opacity":".5"})
 // });
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2,
+            nav:true
+        },
+        1200:{
+        	items:3,
+        	nav:true
+        }
+    }
+})
